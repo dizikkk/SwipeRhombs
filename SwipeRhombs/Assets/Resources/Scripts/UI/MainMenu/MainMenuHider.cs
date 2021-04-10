21556e360113e4b0ee257043f509b7f2db78bae9
Vitaliy Borisovec <vborisovets@playflock.com>
2021-04-10 21:05:23 +0300

    start hider on MainMenu

---
+++ SwipeRhombs/Assets/Resources/Scripts/UI/MainMenu/MainMenuHider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MainMenuHider : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        if (canvasGroup.alpha == 0f)
+            canvasGroup.alpha = 1f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup.DOFade(0f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
